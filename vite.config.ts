01ff10e32f69cd33cbe557939da70de57748b62d
ViNx3h <vinhphuochongnnn@gmail.com>
2024-12-05 13:55:45 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "pokemonHome",
+      remotes: {
+        pokemonList: "http://localhost:5173/assets/remoteEntry.js",
+        pokemonDetail: "http://localhost:5175/assets/remoteEntry1.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
